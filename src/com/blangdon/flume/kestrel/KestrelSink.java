131e9434981dfcc6ec917e95c3a9521e9746bf12
Srinath C <srinath@appcito.net>
2014-01-31 11:01:25 +0530

    Port the flume kestrel code for flume ng version 1.4.0

--- src/com/blangdon/flume/kestrel/KestrelSink.java
+++ src/com/blangdon/flume/kestrel/KestrelSink.java
@@ -1,104 +1,86 @@
 package com.blangdon.flume.kestrel;
 
-
+import com.google.common.base.Throwables;
 import net.rubyeye.xmemcached.MemcachedClient;
-import net.rubyeye.xmemcached.XMemcachedClientBuilder;
 import net.rubyeye.xmemcached.MemcachedClientBuilder;
+import net.rubyeye.xmemcached.XMemcachedClientBuilder;
 import net.rubyeye.xmemcached.command.KestrelCommandFactory;
 import net.rubyeye.xmemcached.utils.AddrUtil;
-import net.rubyeye.xmemcached.exception.MemcachedException;
+import org.apache.flume.Channel;
+import org.apache.flume.Context;
+import org.apache.flume.EventDeliveryException;
+import org.apache.flume.Transaction;
+import org.apache.flume.conf.Configurable;
+import org.apache.flume.sink.AbstractSink;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.TimeoutException;
 
-import com.cloudera.flume.conf.Context;
-import com.cloudera.flume.conf.SinkFactory.SinkBuilder;
-import com.cloudera.flume.core.Event;
-import com.cloudera.flume.core.EventSink;
-import com.cloudera.util.Pair;
-import com.google.common.base.Preconditions;
+import static com.google.common.base.Preconditions.checkNotNull;
 
 /**
  * Sink that sends events to Kestrel queue
  */
-public class KestrelSink extends EventSink.Base {
-	
+public class KestrelSink extends AbstractSink implements Configurable {
+
+    private static final Logger logger = LoggerFactory.getLogger(KestrelSink.class);
 
     private String queue = null;
-    private String servers = null;
     private MemcachedClient client = null;
     private MemcachedClientBuilder builder = null;
 
-    public KestrelSink(String queue, String servers){
-	//constructor
-	this.queue = queue;
-	this.servers = servers;
-
-	this.builder = new XMemcachedClientBuilder( AddrUtil.getAddresses(this.servers) );
-	builder.setCommandFactory(new KestrelCommandFactory());
-
+    @Override
+    public void start() {
+        try {
+            this.client = this.builder.build();
+        } catch (IOException e) {
+            throw Throwables.propagate(e);
+        }
     }
 
     @Override
-	public void open() throws IOException {
-	// Initialized the sink
-	this.client = this.builder.build();
+    public void stop() {
+        try {
+            this.client.shutdown();
+        } catch (IOException e) {
+            throw Throwables.propagate(e);
+        }
     }
-    
-    @Override
-	public void append(Event e) throws IOException {
-	//send to Kestrel
-
-	try{
-	    String message = new String(e.getBody());
-	    this.client.set(this.queue, 0, message);
-	}catch(MemcachedException ex){
-	    throw new IOException("Kestrel Command Failed: " + ex.getMessage());
-	}catch(TimeoutException ex){
-	    throw new IOException("Kestrel Command Timeout: " + ex.getMessage());
-	}catch(InterruptedException ex){
-	    //meh
-	}
-    }
-
 
     @Override
-	public void close() throws IOException {
-	// Cleanup
-	this.client.shutdown();
+    public void configure(Context context) {
+        String servers = checkNotNull(context.getString("servers"),
+                "List of kestrel servers not specified");
+        this.queue = checkNotNull(context.getString("queue"),
+                "Queue name not specified");
+        this.builder = new XMemcachedClientBuilder(AddrUtil.getAddresses(servers));
+        builder.setCommandFactory(new KestrelCommandFactory());
     }
-    
-    public static SinkBuilder builder() {
-	return new SinkBuilder() {
-	    // construct a new parameterized sink
-	    @Override
-		public EventSink build(Context context, String... argv) {
-		Preconditions.checkArgument(argv.length > 1,
-					    "usage: kestrelSink(queueName, server:port, [server2:port, server3:port,...])");
 
-		String servers = "";
-		for( int i = 1; i < argv.length; ++i ){
-		    if( i > 1 ){
-			servers += " ";
-		    }
-		    servers += argv[i];
-		}
-
-		return new KestrelSink(argv[0], servers);
-	    }
-	};
-    }
-    
-    /**
-     * This is a special function used by the SourceFactory to pull in this class
-     * as a plugin sink.
-     */
-    public static List<Pair<String, SinkBuilder>> getSinkBuilders() {
-	List<Pair<String, SinkBuilder>> builders =
-	    new ArrayList<Pair<String, SinkBuilder>>();
-	builders.add(new Pair<String, SinkBuilder>("kestrelSink", builder()));
-	return builders;
+    @Override
+    public Status process() throws EventDeliveryException {
+        Status status = Status.READY;
+        Channel channel = getChannel();
+        Transaction transaction = channel.getTransaction();
+        org.apache.flume.Event nextEvent = null;
+        try {
+            transaction.begin();
+            nextEvent = channel.take();
+            if (nextEvent == null) {
+                status = Status.BACKOFF;
+            } else {
+                String message = new String(nextEvent.getBody());
+                this.client.set(this.queue, 0, message);
+            }
+            transaction.commit();
+        } catch (Exception ex) {
+            transaction.rollback();
+            logger.error("Failed to deliver event: " + nextEvent, ex);
+            throw new EventDeliveryException("Failed to deliver event: " + nextEvent, ex);
+        } finally {
+            transaction.close();
+        }
+        return status;
     }
 }
